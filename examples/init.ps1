0cdea6be101292768d8b99a603a9ce67ec281455
Sander Aernouts <sander.aernouts@gmail.com>
2017-12-29 09:57:44 +0100

    refactored scripts

--- examples/init.ps1
+++ examples/init.ps1
@@ -1,3 +1,4 @@
 #Example init file to be used in your repository
-. { Invoke-WebRequest -useb "https://raw.githubusercontent.com/KDISim/Scripts/latest/InitializeRepository.ps1" } | Invoke-Expression | Out-Nul
-initialize -RequiredTools paket
+. { Invoke-WebRequest -useb "https://raw.githubusercontent.com/KDISim/Scripts/latest/Repository.ps1" } | Invoke-Expression | Out-Nul
+
+Install-Paket
