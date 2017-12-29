feb1c749593f93b94de31b874c3fc93cf7764aea
Sander Aernouts <sander.aernouts@gmail.com>
2017-12-29 09:45:38 +0100

    supressed output from IEX

--- examples/init.ps1
+++ examples/init.ps1
@@ -1,3 +1,3 @@
 #Example init file to be used in your repository
-. { Invoke-WebRequest -useb "https://raw.githubusercontent.com/KDISim/Scripts/latest/InitializeRepository.ps1" } | Invoke-Expression
+. { Invoke-WebRequest -useb "https://raw.githubusercontent.com/KDISim/Scripts/latest/InitializeRepository.ps1" } | Invoke-Expression | Out-Nul
 initialize -RequiredTools paket
